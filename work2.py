7b9b53595abb41dab6ff5b83b39b5cb9ed76bb60
Aleksandr Blinkov <sasha.alem1308@gmail.com>
2022-06-23 15:56:30 +0700

    7

--- work2.py
+++ work2.py
@@ -1,6 +1,9 @@
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 print("10 + 11 = 21, hashaha")
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 =======
